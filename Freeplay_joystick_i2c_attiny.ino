fddc59bea462ff5adeb92b91c0967da6cf3cfbbb
Ed Mandy <freeplay@gmail.com>
2021-12-30 15:29:44 -0700

    LOTS is changing!  input0, input1 are laid out different.  ADC0-3 are always compiled in, DPAD is done via ADC18

--- Freeplay_joystick_i2c_attiny.ino
+++ Freeplay_joystick_i2c_attiny.ino
@@ -13,7 +13,7 @@
 #include <EEPROM.h>
 
 #define VERSION_MAJOR   0
-#define VERSION_MINOR   6
+#define VERSION_MINOR   7
 
 
 // Firmware for the ATtiny817/ATtiny427/etc. to emulate the behavior of the PCA9555 i2c GPIO Expander
@@ -23,37 +23,29 @@
 #define CONFIG_I2C_ADDR     0x20
 #define CONFIG_I2C_2NDADDR  0x40  //0x30 wouldn't work
 
-//#define CONFIG_SERIAL_DEBUG  //shares pins with L2/R2 IO1_6/IO1_7
+//#define CONFIG_SERIAL_DEBUG  //shares pins with L2/R2 IO1_4/IO1_5
 
-//#define USE_INTERRUPTS
-#define USE_ADC
+#define USE_INTERRUPTS
 
-#ifdef USE_ADC
- //#define ADC_RESOLUTION 10
- //#define ADC_RESOLUTION 12  //can do analogReadResolution(12) on 2-series 427/827 chips
- #define ADC_RESOLUTION ADC_NATIVE_RESOLUTION
- #define USE_ADC0
- #define USE_ADC1
- #define USE_ADC2
- #define USE_ADC3
- #define USE_ADC18
-#else
- #define ADC_RESOLUTION 0
-#endif
+//#define ADC_RESOLUTION 10
+//#define ADC_RESOLUTION 12  //can do analogReadResolution(12) on 2-series 427/827 chips
+#define ADC_RESOLUTION ADC_NATIVE_RESOLUTION
+#define USE_ADC0
+#define USE_ADC1
+#define USE_ADC2
+#define USE_ADC3
 
 
-#if defined(USE_ADC0) && defined(USE_INTERRUPTS)
- #error ADC0 and nINT_PIN share the same pin
-#endif
 
-//I feel like we only want to do interrupts if we're not doing ADCs, but this can change around
-#if !defined(USE_ADC0) && defined(USE_INTERRUPTS)
- #define nINT_PIN 0 //also PIN_PA4
- bool g_nINT_state = false;
-#endif
+//Interrupts are only really useful for digital inputs, so the nINT_PIN will go low only for input0 and input1 changes
+#define nINT_PIN 19 //also PIN_PA4
+bool g_nINT_state = false;
 
 
 #define POWEROFF_OUT_PIN 8 //PIN_PB3  //pin num 8
+#define PIN_PWM             20
+
+
 
 #define CONFIG_INVERT_POWER_BUTTON      //power button is on PIN_PB3
 
@@ -105,97 +97,70 @@ volatile byte g_i2c_command_index = 0; //Gets set when user writes an address. W
 byte g_pwm_step = 0x00;  //100% on
 
 /*
- * PA1 = IO0_0 = UP
- * PA2 = IO0_1 = DOWN
- * PB4 = IO0_2 = LEFT
- * PB5 = IO0_3 = RIGHT
- * PB6 = IO0_4 = BTN_A
- * PB7 = IO0_5 = BTN_B
- * PA6 = IO0_6 = BTN_L2  ifndef USE_ADC2
- * PA7 = IO0_7 = BTN_R2  ifndef USE_ADC3
  * 
- * PC0 = IO1_0 = BTN_X
- * PC1 = IO1_1 = BTN_Y
- * PC2 = IO1_2 = BTN_START
- * PC3 = IO1_3 = BTN_SELECT
- * PC4 = IO1_4 = BTN_L
- * PC5 = IO1_5 = BTN_R
- * PB2 = IO1_6 = POWER_BUTTON (Hotkey AKA poweroff_in)   ifndef CONFIG_SERIAL_DEBUG (or can be used for UART TXD0 for debugging)
- * PA5 = IO1_7 = BTN_C ifndef USE_ADC1
+ * PC0 = IO0_0 = BTN_X
+ * PC1 = IO0_1 = BTN_Y
+ * PC2 = IO0_2 = BTN_START
+ * PC3 = IO0_3 = BTN_SELECT
+ * PC4 = IO0_4 = BTN_L
+ * PC5 = IO0_5 = BTN_R
+ * PB6 = IO0_6 = BTN_A
+ * PB7 = IO0_7 = BTN_B
+ * 
+ * A18 means analog pin 18
+ * 
+ * A18 = IO1_0 = UP
+ * A18 = IO1_1 = DOWN
+ * A18 = IO1_2 = LEFT
+ * A18 = IO1_3 = RIGHT
+ * PB2 = IO1_4 = BTN_L2 (in debug mode, can be used for serial TXD)
+ * PB3 = IO1_5 = BTN_R2 (in debug mode, can be used for serial RXD)
+ * PB4 = IO1_6 = POWER_BUTTON (Hotkey AKA poweroff_in)
+ * ___ = IO1_7 = HIGH (logic 1)
+ * 
  * 
  * PB3 =         POWEROFF_OUT
  * PA3 =         PWM Backlight OUT
+ * PA2 =         nINT OUT
  * 
  */
 
-#define INPUT0_DPAD_UP    (1 << 0)      //IO0_0
-#define INPUT0_DPAD_DOWN  (1 << 1)      //IO0_1
-#define INPUT0_BTN_A      (1 << 4)      //IO0_4
-#define INPUT0_BTN_B      (1 << 5)      //IO0_5
-#define INPUT1_BTN_L      (1 << 4)      //IO1_4
-#define INPUT1_BTN_R      (1 << 5)      //IO1_5
+
+#define INPUT0_BTN_X      (1 << 0)      //IO0_0
+#define INPUT0_BTN_Y      (1 << 1)      //IO0_1
+#define INPUT0_BTN_START  (1 << 2)      //IO0_2
+#define INPUT0_BTN_SELECT (1 << 3)      //IO0_3
+#define INPUT0_BTN_L      (1 << 4)      //IO0_4
+#define INPUT0_BTN_R      (1 << 5)      //IO0_5
+#define INPUT0_BTN_A      (1 << 6)      //IO0_6
+#define INPUT0_BTN_B      (1 << 7)      //IO0_7
+
+#define INPUT1_DPAD_UP    (1 << 0)      //IO1_0
+#define INPUT1_DPAD_DOWN  (1 << 1)      //IO1_1
+#define INPUT1_DPAD_LEFT  (1 << 2)      //IO1_2
+#define INPUT1_DPAD_RIGHT (1 << 3)      //IO1_3
+#define INPUT1_BTN_L2     (1 << 4)      //IO1_4
+#define INPUT1_BTN_R2     (1 << 5)      //IO1_5
 #define INPUT1_BTN_POWER  (1 << 6)      //IO1_6
+                                        //IO1_7
 
-
-
-#define PINA_DPAD_UP    (1 << 1)      //PA1
-#define PINA_DPAD_DOWN  (1 << 2)      //PA2
-#define PINA_BTN_C      (1 << 5)      //PA5
-#define PINA_BTN_L2     (1 << 6)      //PA6
-#define PINA_BTN_R2     (1 << 7)      //PA7
-
-#define PINB_BTN_PWR    (1 << 2)      //PB2
-#define PINB_DPAD_LEFT  (1 << 4)      //PB4
-#define PINB_DPAD_RIGHT (1 << 5)      //PB5
-#define PINB_BTN_A      (1 << 6)      //PB6
-#define PINB_BTN_B      (1 << 7)      //PB7
-
-#define PINC_BTN_X      (1 << 0)      //PC0
-#define PINC_BTN_Y      (1 << 1)      //PC1
-#define PINC_BTN_START  (1 << 2)      //PC2
-#define PINC_BTN_SELECT (1 << 3)      //PC3
-#define PINC_BTN_L      (1 << 4)      //PC4
-#define PINC_BTN_R      (1 << 5)      //PC5
-
+#define PINB_POWER_BUTTON (1 << 4)    //PB4 is the power button, but it needs to be inverted (high when pressed)
+                                        
 
 //PRESSED means that the bit is 0
-#define IS_PRESSED_PINB_BTN_A(pinb_test) ((pinb_test & PINB_BTN_A) != PINB_BTN_A)
-#define IS_PRESSED_PINB_BTN_B(pinb_test) ((pinb_test & PINB_BTN_B) != PINB_BTN_B)
-
-#define IS_PRESSED_PINC_BTN_L(pinc_test) ((pinc_test & PINC_BTN_L) != PINC_BTN_L)
-#define IS_PRESSED_PINC_BTN_R(pinc_test) ((pinc_test & PINC_BTN_R) != PINC_BTN_R)
-
-#define IS_PRESSED_PINA_DPAD_UP(pina_test) ((pina_test & PINA_DPAD_UP) != PINA_DPAD_UP)
-#define IS_PRESSED_PINA_DPAD_DOWN(pina_test) ((pina_test & PINA_DPAD_DOWN) != PINA_DPAD_DOWN)
-
-
-#define IS_PRESSED_DPAD_UP() ((i2c_joystick_registers.input0 & INPUT0_DPAD_UP) != INPUT0_DPAD_UP)
-#define IS_PRESSED_DPAD_DOWN() ((i2c_joystick_registers.input0 & INPUT0_DPAD_DOWN) != INPUT0_DPAD_DOWN)
+#define IS_PRESSED_DPAD_UP() ((i2c_joystick_registers.input1 & INPUT1_DPAD_UP) != INPUT1_DPAD_UP)
+#define IS_PRESSED_DPAD_DOWN() ((i2c_joystick_registers.input1 & INPUT1_DPAD_DOWN) != INPUT1_DPAD_DOWN)
 
 #define IS_PRESSED_BTN_A() ((i2c_joystick_registers.input0 & INPUT0_BTN_A) != INPUT0_BTN_A)
 #define IS_PRESSED_BTN_B() ((i2c_joystick_registers.input0 & INPUT0_BTN_B) != INPUT0_BTN_B)
 
-#define IS_PRESSED_BTN_L() ((i2c_joystick_registers.input1 & INPUT1_BTN_L) != INPUT1_BTN_L)
-#define IS_PRESSED_BTN_R() ((i2c_joystick_registers.input1 & INPUT1_BTN_R) != INPUT1_BTN_R)
+#define IS_PRESSED_BTN_L() ((i2c_joystick_registers.input0 & INPUT0_BTN_L) != INPUT0_BTN_L)
+#define IS_PRESSED_BTN_R() ((i2c_joystick_registers.input0 & INPUT0_BTN_R) != INPUT0_BTN_R)
 
 #define IS_PRESSED_BTN_POWER() ((i2c_joystick_registers.input1 & INPUT1_BTN_POWER) != INPUT1_BTN_POWER)
 
 #define IS_SPECIAL_INPUT_MODE() (IS_PRESSED_BTN_A() && IS_PRESSED_BTN_B() && IS_PRESSED_BTN_L() && IS_PRESSED_BTN_R())
 
-
-
-
-#define PIN_POWEROFF_OUT   1
-#define PIN_PWM             20
-
-
-
-
-//if you change any of these, then you need to change the pullups in setup()
-//#define PINA_ADC0_MASK      (0b00010000)    //don't need this, because PA4 is never a digital BTN input (only nINT or ADC0)
-#define PINA_ADC1_MASK      (0b00100000)
-#define PINA_ADC2_MASK      (0b01000000)
-#define PINA_ADC3_MASK      (0b10000000)
 
 #ifdef CONFIG_SERIAL_DEBUG
  #define PINB_UART_MASK     (0b00001100)
@@ -203,34 +168,24 @@ byte g_pwm_step = 0x00;  //100% on
  #define PINB_UART_MASK     (0b00000000)
 #endif
 
-#define PINA_IN0_MASK      (0b00000110)   //the pins from PINA that are used in IN0     (not counting ADC pins)
-#define PINB_IN0_MASK      (0b11110000)   //the pins from PINB that are used in IN0
 
-#ifdef USE_ADC1
-#define PINA_IN1_MASK      (0b00000000)   //the pins from PINA that are used in IN1
-#else
-#define PINA_IN1_MASK      (0b00100000)   //the pins from PINA that are used in IN1
-#endif
-#define PINB_IN1_MASK      (0b00000100)   //the pins from PINB that are used in IN1
-#define PINC_IN1_MASK      (0b00111111)   //the pins from PINC that are used in IN1
+#define PINB_IN0_MASK      (0b11000000)   //the pins from PINB that are used in IN0
+#define PINC_IN0_MASK      (0b00111111)   //the pins from PINC that are used in IN0
 
-#define PINA_GPIO_MASK     (PINA_IN0_MASK)
+#define PINB_IN1_MASK      (0b00011100)   //the pins from PINB that are used in IN1
+
 #define PINB_GPIO_MASK     ((PINB_IN0_MASK | PINB_IN1_MASK) & ~PINB_UART_MASK)
-#define PINC_GPIO_MASK     (PINC_IN1_MASK)
+#define PINC_GPIO_MASK     (PINC_IN0_MASK)
 
-#define PINB_POWER_BUTTON (1 << 2)    //PB2 is the power button, but it needs to be inverted (high when pressed)
+#define PINB_IN1_SHL       2
 
-#define PINA_IN0_SHR       1
-#define PINB_IN0_SHR       2
-#define PINB_IN1_SHL       4
-#define PINA_IN1_SHL       2
+#define PINA_ADC_MASK      (0b11110010)   //the pins in port A used for ADC
 
 void setup_gpio(void)
 {
   //set as inputs
 
-  PORTA_DIRCLR = PINA_GPIO_MASK | /*PINA_ADC0_MASK |*/ PINA_ADC1_MASK | PINA_ADC2_MASK | PINA_ADC3_MASK;
-
+  PORTA_DIRCLR = PINA_ADC_MASK;
   PORTB_DIRCLR = PINB_GPIO_MASK;
   PORTC_DIRCLR = PINC_GPIO_MASK;
 
@@ -238,11 +193,9 @@ void setup_gpio(void)
 
 //#define PINA_MASK (0b00001110) for ADC
   //PORTA_PIN0CTRL = PORT_PULLUPEN_bm;
-#ifndef USE_ADC18
-  PORTA_PIN1CTRL = PORT_PULLUPEN_bm;
-#endif
-  PORTA_PIN2CTRL = PORT_PULLUPEN_bm;
-  PORTA_PIN3CTRL = PORT_PULLUPEN_bm;
+  //PORTA_PIN1CTRL = PORT_PULLUPEN_bm;    //PA1 is used for analog
+  //PORTA_PIN2CTRL = PORT_PULLUPEN_bm;    //PA2 = nINT output
+  //PORTA_PIN3CTRL = PORT_PULLUPEN_bm;    //PA3 = PWM output
 #ifndef USE_ADC0
   PORTA_PIN4CTRL = PORT_PULLUPEN_bm; //ADC0
 #endif
@@ -256,17 +209,16 @@ void setup_gpio(void)
   PORTA_PIN7CTRL = PORT_PULLUPEN_bm; //ADC3
 #endif
   
-//#define PINB_MASK (0b01110000)
-  //PORTB_PIN0CTRL = PORT_PULLUPEN_bm;
-  //PORTB_PIN1CTRL = PORT_PULLUPEN_bm;
+  //PORTB_PIN0CTRL = PORT_PULLUPEN_bm;    //i2c
+  //PORTB_PIN1CTRL = PORT_PULLUPEN_bm;    //i2c
 #ifndef CONFIG_SERIAL_DEBUG
- #if !defined(CONFIG_INVERT_POWER_BUTTON)    //don't use a pullup on the power button
   PORTB_PIN2CTRL = PORT_PULLUPEN_bm;
- #endif
   PORTB_PIN3CTRL = PORT_PULLUPEN_bm;
 #endif
+#if !defined(CONFIG_INVERT_POWER_BUTTON)    //don't use a pullup on the power button
   PORTB_PIN4CTRL = PORT_PULLUPEN_bm;
-  PORTB_PIN5CTRL = PORT_PULLUPEN_bm;
+#endif
+  //PORTB_PIN5CTRL = PORT_PULLUPEN_bm;    //PB5 = PowerOff_OUT_attiny
   PORTB_PIN6CTRL = PORT_PULLUPEN_bm;
   PORTB_PIN7CTRL = PORT_PULLUPEN_bm;
 
@@ -293,103 +245,76 @@ void read_all_gpio(void)
   //then set up the input registers as TEMPORARY registers
   //then put them in the actual i2c_joystick_registers 
   //otherwise, if we get interrupted during the setup, things get out of whack
-  
-  byte input0, input1;
-  byte pa_in = PORTA_IN;
-  byte pb_in = PORTB_IN;
-  byte pc_in = PORTC_IN;
+
+  uint8_t rldu = 0b1111;
+  uint8_t input0, input1;
+  uint8_t pb_in = PORTB_IN;
+  uint8_t pc_in = PORTC_IN;
 
 #ifdef CONFIG_INVERT_POWER_BUTTON
- pb_in ^= PINB_POWER_BUTTON;
+  pb_in ^= PINB_POWER_BUTTON;
 #endif
-
-  input0 = ((pa_in & PINA_IN0_MASK) >> PINA_IN0_SHR) | ((pb_in & PINB_IN0_MASK) >> PINB_IN0_SHR);
-
-#ifndef CONFIG_SERIAL_DEBUG
-  input1 = ((pb_in & PINB_IN1_MASK) << PINB_IN1_SHL) |  (pc_in & PINC_IN1_MASK) | ((pa_in & PINA_IN1_MASK) << PINA_IN1_SHL);
- #ifdef USE_ADC1
-  input1 |= (PINA_ADC1_MASK << PINA_IN1_SHL);
- #endif  
-#else
-  input1 = ((pb_in & (PINB_IN1_MASK | PINB_UART_MASK)) << PINB_IN1_SHL) |  (pc_in & PINC_IN1_MASK) | ((pa_in & PINA_IN1_MASK) << PINA_IN1_SHL);   //act like the PINB_UART_MASK pins are never pressed
-#endif
-
-#if defined(USE_ADC2) && defined(USE_ADC3)
-  input0 |= (PINA_ADC3_MASK | PINA_ADC2_MASK);
-#elif !defined(USE_ADC2) && !defined(USE_ADC3)
-  input0 |= (pa_in & (PINA_ADC2_MASK | PINA_ADC3_MASK));
-#elif defined(USE_ADC3) && !defined(USE_ADC2)
-  input0 |= (pa_in & PINA_ADC2_MASK) | PINA_ADC3_MASK;
-#elif defined(USE_ADC2) && !defined(USE_ADC3)
-  input0 |= (pa_in & PINA_ADC3_MASK) | PINA_ADC2_MASK;
-#else
- #error How can this be?
-#endif
-
-
-#if defined(USE_ADC18)
-#warning TESTING USE_ADC18/19
 
   uint16_t adc18 = analogRead(18);
-  
-  uint8_t ulrd = 0;
-  uint8_t rldu = 0;
 
   //NOTE:  This code will not let you press Up&Down or Left&Right at the same time
 
   if(adc18 < 65)
   {
-    ulrd = 0b1111;
+    //no dpad pressed
     rldu = 0b1111;
   }
   else if(adc18 < 131)
   {
-    ulrd = 0b1110;
+    //down
     rldu = 0b1101;
   }
   else if(adc18 < 201)
   {
-    ulrd = 0b1101;
+    //right
     rldu = 0b0111;
   }
   else if(adc18 < 260)
   {
-    ulrd = 0b1100;
+    //down and to the right
     rldu = 0b0101;
   }  
   else if(adc18 < 313)
   {
-    ulrd = 0b1011;
+    //left
     rldu = 0b1011;
   }  
   else if(adc18 < 398)
   {
-    ulrd = 0b1010;
+    //down and to the left
     rldu = 0b1001;
   }  
   else if(adc18 < 536)
   {
-    ulrd = 0b0111;
+    //up
     rldu = 0b1110;
   }    
   else if(adc18 < 578)
   {
-    ulrd = 0b0101;
+    //up and to the right
     rldu = 0b0110;
   }
   else
   {
     //up and to the left
-    ulrd = 0b0011;
     rldu = 0b1010;
   }
 
 
+  input0 = (pc_in & PINC_IN0_MASK) | (pb_in & PINB_IN0_MASK);
 
-  input0 &= 0b11110000;
-  input0 |= rldu;
-
+#ifndef CONFIG_SERIAL_DEBUG
+  input1 = ((pb_in & PINB_IN1_MASK) << PINB_IN1_SHL) | rldu | (1<<7);
+#else
+  input1 = ((pb_in & (PINB_IN1_MASK | PINB_UART_MASK)) << PINB_IN1_SHL) | rldu | (1<<7));   //act like the PINB_UART_MASK pins are never pressed
 #endif
+
+
 
 
   i2c_joystick_registers.input0 = input0;
@@ -681,9 +606,7 @@ void setup()
 void loop() 
 {
   
-#ifdef USE_ADC
   word adc;
-#endif
 
 #ifdef CONFIG_SERIAL_DEBUG
   Serial.print("g_i2c_address=");
@@ -719,10 +642,6 @@ void loop()
   if(i2c_joystick_registers.adc_conf_bits & (1 << 0))
   {
     adc = analogRead(PIN_PA4);
-    #ifdef USE_ADC18
-    #warning DONT REALLY DO THIS
-    adc = analogRead(18);
-    #endif
     i2c_joystick_registers.a0_msb = adc >> (ADC_RESOLUTION - 8);
     i2c_joystick_registers.a1a0_lsb = (adc << (4 - (ADC_RESOLUTION - 8)) & 0x0F) | (i2c_joystick_registers.a1a0_lsb & 0xF0);
   }
