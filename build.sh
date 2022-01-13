e6d013cc3e67ef2944f643ea1369cc208f5b98b8
TheFlav <freeplay@gmail.com>
2022-01-13 20:25:18 +0000

    Adding scripts for building on RPi

---
+++ build.sh
@@ -0,0 +1,1 @@
+arduino-cli compile -v -b megaTinyCore:megaavr:atxy7:chip=817,clock=10internal,bodvoltage=1v8,bodmode=disabled,eesave=enable,millis=enabled,resetpin=UPDI,startuptime=0 --output-dir ./build/
---
+++ ping_attiny.sh
@@ -0,0 +1,11 @@
+sudo systemctl stop hciuart.service 
+sudo systemctl stop bluetooth.service 
+sudo rmmod hci_uart 
+sudo rmmod bnep btbcm bluetooth
+raspi-gpio set 30-33 ip
+raspi-gpio set 14 a0
+raspi-gpio set 15 a0 pu
+
+raspi-gpio set 11 op dh
+
+pymcuprog -d attiny817 -t uart -u /dev/ttyAMA0 ping
