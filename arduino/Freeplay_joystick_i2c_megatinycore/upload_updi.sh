81f0b6de8029d5931c716fb27573f35a0ea3f9d6
Ed Mandy <freeplay@gmail.com>
2022-04-05 22:53:40 +0100

    Updates for INO building/uploading

--- arduino/Freeplay_joystick_i2c_megatinycore/upload_updi.sh
+++ arduino/Freeplay_joystick_i2c_megatinycore/upload_updi.sh
@@ -7,4 +7,11 @@ raspi-gpio set 14 a0
 raspi-gpio set 15 a0 pu
 raspi-gpio set 11 op dh
 
-make upload
+
+if [ "${CHIP}" != "817" ]; then
+  CHIP=1627
+  echo Setting CHIP as ${CHIP}
+else
+  echo Using CHIP as ${CHIP}
+fi
+bin/arduino-cli upload -b megaTinyCore:megaavr:atxy7:chip=${CHIP},clock=10internal,bodvoltage=1v8,bodmode=disabled,eesave=enable,millis=enabled,resetpin=UPDI,startuptime=0 -p /dev/ttyAMA0 -P serialupdi -t --input-dir ./Freeplay_joystick_i2c_megatinycore_build_${CHIP}/
