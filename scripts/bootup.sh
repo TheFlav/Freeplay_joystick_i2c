f1399687dc0eea8ab672d59fcbce7bc6c9fe80f3
Ed <freeplay@gmail.com>
2022-04-08 12:29:10 -0600

    Adding gamepad startup to bootup.sh

--- scripts/bootup.sh
+++ scripts/bootup.sh
@@ -1,14 +1,51 @@
 #!/bin/bash
-FPJSMAGIC=`i2cget -y 1 0x30 0x0D`
-FPJS2NDMAGIC=`i2cget -y 1 0x40 0x0D`
 
 #power button GPIO pin
 GPIO=27
 
+sudo /home/pi/Freeplay/Freeplay_joystick_i2c/uhid/gamepad/uhid-i2c-gamepad -confignocreate -closeonwarn -inputsearch 0x13b
 
-if [ "${FPJSMAGIC}" != "0xed" ] || [ "${FPJS2NDMAGIC}" != "0xed" ]; then
+GAMEPAD_RETURN_CODE=$?
+
+JOY_NEEDS_NEW_FIRMWARE=0
+
+if [ $GAMEPAD_RETURN_CODE -ne 0 ]; then
+	echo "Freeplay i2c Joystick status code $GAMEPAD_RETURN_CODE needs attention!"
+	if [ $GAMEPAD_RETURN_CODE -eq 1 ]; then
+		echo "START button detected.  Entering Freeplay i2c Joystick configuration tool."
+		sudo /home/pi/Freeplay/Freeplay_joystick_i2c/uhid/gamepad/uhid-i2c-gamepad-diag
+	fi
+	if [ $GAMEPAD_RETURN_CODE -eq -5 ]; then
+		echo "Freeplay i2c Joystick not configured.  Entering Freeplay i2c Joystick configuration tool."
+		sudo /home/pi/Freeplay/Freeplay_joystick_i2c/uhid/gamepad/uhid-i2c-gamepad-diag
+	fi
+	if [ $GAMEPAD_RETURN_CODE -eq -4 ]; then
+		echo "Freeplay i2c Joystick version mismatch.  Firmware update required."
+		JOY_NEEDS_NEW_FIRMWARE=1
+	fi
+	if [ $GAMEPAD_RETURN_CODE -eq -3 ]; then
+		echo "Freeplay i2c Joystick manufacturer mismatch.  Firmware update required."
+		JOY_NEEDS_NEW_FIRMWARE=1
+	fi
+	if [ $GAMEPAD_RETURN_CODE -eq -2 ]; then
+		echo "Freeplay i2c Joystick not found on /dev/i2c-1.  Firmware update required or i2c-1 not connected/configured properly."
+		JOY_NEEDS_NEW_FIRMWARE=1
+	fi
+fi
+
+
+
+#uncomment this section to use i2cget for Joystick firmware verification
+#FPJSMAGIC=`i2cget -y 1 0x30 0x0D`
+#FPJS2NDMAGIC=`i2cget -y 1 0x40 0x0D`
+#if [ "${FPJSMAGIC}" != "0xed" ] || [ "${FPJS2NDMAGIC}" != "0xed" ]; then
+#	JOY_NEEDS_NEW_FIRMWARE=1
+#fi
+
+
+if [ $JOY_NEEDS_NEW_FIRMWARE -eq 1 ]; then
 	#echo "FAILED FPJSMAGIC=${FPJSMAGIC}, FPJS2NDMAGIC=${FPJS2NDMAGIC}"
-	echo "Freeplay i2c Joystick device not found on i2c-1!"
+	echo "Freeplay i2c Joystick needs new firmware."
 
 	#cd /home/pi/Freeplay/Freeplay_joystick_i2c/arduino/Freeplay_joystick_i2c_megatinycore
         #./verify.sh
@@ -50,8 +87,30 @@ if [ "${FPJSMAGIC}" != "0xed" ] || [ "${FPJS2NDMAGIC}" != "0xed" ]; then
 		echo "Uploading firmware via UPDI."
 		cd /home/pi/Freeplay/Freeplay_joystick_i2c/arduino/Freeplay_joystick_i2c_megatinycore
 		./upload_updi.sh
-		sudo reboot
+
+	        SKIP=0
+        	let SECONDSTOWAIT=10
+	        STARTTIME=`date +%s`
+	        CURRTIME=`date +%s`
+	        let ELAPSED=${CURRTIME}-${STARTTIME}
+	        while [ "${SKIP}" == "0" ] && (( ELAPSED < SECONDSTOWAIT )); do
+	                if [ 1 == "$(</sys/class/gpio/gpio"${GPIO}"/value)" ]; then
+	                        SKIP=1
+	                fi
+	                CURRTIME=`date +%s`
+	                let ELAPSED=${CURRTIME}-${STARTTIME}
+	                if (( ELAPSED != PREVELAPSED)); then
+	                        let SECLEFT=${SECONDSTOWAIT}-${ELAPSED}
+	                        echo "$SECLEFT seconds left to tap and release POWER button to skip reboot."
+	                fi
+	                let PREVELAPSED=${ELAPSED}
+	        done
+	        if [ "${SKIP}" == "0" ]; then
+			sudo reboot
+		fi
 	fi
 fi
 
 echo "Freeplay i2c Joystick Found!"
+echo "Freeplay i2c Joystick driver starting."
+sudo /home/pi/Freeplay/Freeplay_joystick_i2c/uhid/gamepad/uhid-i2c-gamepad > /dev/null &
