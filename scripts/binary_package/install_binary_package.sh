dafd706e90c50c036e68832daa3be77b4f2c7e5e
Ed <freeplay@gmail.com>
2022-04-20 17:19:28 -0600

    Adding some extra setup stuff

--- scripts/binary_package/install_binary_package.sh
+++ scripts/binary_package/install_binary_package.sh
@@ -1,4 +1,4 @@
-
+#!/bin/bash
 mkdir -p /home/pi/Freeplay/Freeplay_joystick_i2c/arduino/Freeplay_joystick_i2c_megatinycore/Freeplay_joystick_i2c_megatinycore_build_1627/
 cp contents/Freeplay_joystick_i2c_megatinycore.ino.hex /home/pi/Freeplay/Freeplay_joystick_i2c/arduino/Freeplay_joystick_i2c_megatinycore/Freeplay_joystick_i2c_megatinycore_build_1627/Freeplay_joystick_i2c_megatinycore.ino.hex
 mkdir -p /home/pi/Freeplay/Freeplay_joystick_i2c/uhid/gamepad
@@ -6,4 +6,5 @@ cp contents/uhid-i2c-gamepad /home/pi/Freeplay/Freeplay_joystick_i2c/uhid/gamepa
 cp contents/uhid-i2c-gamepad-diag /home/pi/Freeplay/Freeplay_joystick_i2c/uhid/gamepad
 mkdir -p /home/pi/Freeplay/Freeplay_joystick_i2c/scripts
 cp contents/bootup.sh /home/pi/Freeplay/Freeplay_joystick_i2c/scripts
-contents/install_to_retropie.sh 
+contents/install_to_retropie.sh
+contents/setup_linux.sh
--- scripts/setup_linux.sh
+++ scripts/setup_linux.sh
@@ -1,1 +1,5 @@
 sudo apt install i2c-tools libi2c-dev wiringpi
+if ! grep -Fxq "/etc/modules" i2c-dev; then
+	sed -i '1 i /etc/modules' i2c-dev
+fi
+
