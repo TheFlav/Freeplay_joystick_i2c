4605ede655c5551b0809628b82d7cbb15be88512
Ed <freeplay@gmail.com>
2022-04-20 16:52:45 -0600

    Fix to add bootup.sh script

--- scripts/binary_package/install_binary_package.sh
+++ scripts/binary_package/install_binary_package.sh
@@ -4,4 +4,6 @@ cp contents/Freeplay_joystick_i2c_megatinycore.ino.hex /home/pi/Freeplay/Freepla
 mkdir -p /home/pi/Freeplay/Freeplay_joystick_i2c/uhid/gamepad
 cp contents/uhid-i2c-gamepad /home/pi/Freeplay/Freeplay_joystick_i2c/uhid/gamepad
 cp contents/uhid-i2c-gamepad-diag /home/pi/Freeplay/Freeplay_joystick_i2c/uhid/gamepad
+mkdir -p /home/pi/Freeplay/Freeplay_joystick_i2c/scripts
+cp contents/bootup.sh /home/pi/Freeplay/Freeplay_joystick_i2c/scripts
 contents/install_to_retropie.sh 
