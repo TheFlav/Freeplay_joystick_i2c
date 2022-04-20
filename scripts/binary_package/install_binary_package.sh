eef2ef87edc054059852f82dc30e6ade64211954
Ed <freeplay@gmail.com>
2022-04-20 16:47:07 -0600

    Fix for contents

--- scripts/binary_package/install_binary_package.sh
+++ scripts/binary_package/install_binary_package.sh
@@ -1,7 +1,7 @@
 
 mkdir -p /home/pi/Freeplay/Freeplay_joystick_i2c/arduino/Freeplay_joystick_i2c_megatinycore/Freeplay_joystick_i2c_megatinycore_build_1627/
-cp Freeplay_joystick_i2c_megatinycore.ino.hex /home/pi/Freeplay/Freeplay_joystick_i2c/arduino/Freeplay_joystick_i2c_megatinycore/Freeplay_joystick_i2c_megatinycore_build_1627/Freeplay_joystick_i2c_megatinycore.ino.hex
+cp contents/Freeplay_joystick_i2c_megatinycore.ino.hex /home/pi/Freeplay/Freeplay_joystick_i2c/arduino/Freeplay_joystick_i2c_megatinycore/Freeplay_joystick_i2c_megatinycore_build_1627/Freeplay_joystick_i2c_megatinycore.ino.hex
 mkdir -p /home/pi/Freeplay/Freeplay_joystick_i2c/uhid/gamepad
-cp uhid-i2c-gamepad /home/pi/Freeplay/Freeplay_joystick_i2c/uhid/gamepad
-cp uhid-i2c-gamepad-diag /home/pi/Freeplay/Freeplay_joystick_i2c/uhid/gamepad
-./install_to_retropie.sh 
+cp contents/uhid-i2c-gamepad /home/pi/Freeplay/Freeplay_joystick_i2c/uhid/gamepad
+cp contents/uhid-i2c-gamepad-diag /home/pi/Freeplay/Freeplay_joystick_i2c/uhid/gamepad
+contents/install_to_retropie.sh 
