a0a024b74918dc92d947068eec187f46f2de954c
Ed Mandy <freeplay@gmail.com>
2022-04-21 15:06:57 -0600

    Update install_binary_package.sh

--- scripts/binary_package/install_binary_package.sh
+++ scripts/binary_package/install_binary_package.sh
@@ -1,10 +1,22 @@
-#!/bin/bash
-mkdir -p /home/pi/Freeplay/Freeplay_joystick_i2c/arduino/Freeplay_joystick_i2c_megatinycore/Freeplay_joystick_i2c_megatinycore_build_1627/
-cp contents/Freeplay_joystick_i2c_megatinycore.ino.hex /home/pi/Freeplay/Freeplay_joystick_i2c/arduino/Freeplay_joystick_i2c_megatinycore/Freeplay_joystick_i2c_megatinycore_build_1627/Freeplay_joystick_i2c_megatinycore.ino.hex
-mkdir -p /home/pi/Freeplay/Freeplay_joystick_i2c/uhid/gamepad
-cp contents/uhid-i2c-gamepad /home/pi/Freeplay/Freeplay_joystick_i2c/uhid/gamepad
-cp contents/uhid-i2c-gamepad-diag /home/pi/Freeplay/Freeplay_joystick_i2c/uhid/gamepad
-mkdir -p /home/pi/Freeplay/Freeplay_joystick_i2c/scripts
-cp contents/bootup.sh /home/pi/Freeplay/Freeplay_joystick_i2c/scripts
-contents/install_to_retropie.sh
-contents/setup_linux.sh
+#!/usr/bin/env bash
+
+SOURCE=${BASH_SOURCE[0]}
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  SCRIPTDIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
+  SOURCE=$(readlink "$SOURCE")
+  [[ $SOURCE != /* ]] && SOURCE=$DIR/$SOURCE # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+SCRIPTDIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
+CONTENTSDIR=$SCRIPTDIR/contents
+
+DESTDIR=/home/pi/Freeplay/Freeplay_joystick_i2c/
+
+mkdir -p $DESTDIR/arduino/Freeplay_joystick_i2c_megatinycore/Freeplay_joystick_i2c_megatinycore_build_1627/
+cp $CONTENTSDIR/Freeplay_joystick_i2c_megatinycore.ino.hex $DESTDIR/arduino/Freeplay_joystick_i2c_megatinycore/Freeplay_joystick_i2c_megatinycore_build_1627/
+mkdir -p $DESTDIR/uhid/gamepad
+cp $CONTENTSDIR/uhid-i2c-gamepad $DESTDIR/uhid/gamepad
+cp $CONTENTSDIR/uhid-i2c-gamepad-diag $DESTDIR/uhid/gamepad
+mkdir -p $DESTDIR/scripts
+cp $CONTENTSDIR/bootup.sh $DESTDIR/scripts
+$CONTENTSDIR/install_to_retropie.sh
+$CONTENTSDIR/setup_linux.sh
