38cf29ef0211ddde1b5ca6da4612bb7efee307fa
Ed <freeplay@gmail.com>
2022-04-21 13:46:36 -0600

    testing firstboot stuff

--- scripts/setup_linux.sh
+++ scripts/setup_linux.sh
@@ -1,6 +1,6 @@
 sudo raspi-config nonint do_i2c 0
-sudo apt install i2c-tools libi2c-dev wiringpi
-if ! grep -Fxq "/etc/modules" i2c-dev; then
-	sed -i '1 i /etc/modules' i2c-dev
-fi
+#sudo apt install i2c-tools libi2c-dev wiringpi
+#if ! grep -Fxq "/etc/modules" i2c-dev; then
+#	sed -i '1 i /etc/modules' i2c-dev
+#fi
 
