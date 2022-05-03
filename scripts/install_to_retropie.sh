3cb8dc12aadf9b39abd61811fda2d89d45b3357c
Ed <freeplay@gmail.com>
2022-05-03 11:40:05 -0600

    moving lcd on/off/dim scripts

--- scripts/install_to_retropie.sh
+++ scripts/install_to_retropie.sh
@@ -1,16 +1,21 @@
 #!/usr/bin/env bash
 
-SOURCE=${BASH_SOURCE[0]}
-while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
-  SCRIPTDIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
-  SOURCE=$(readlink "$SOURCE")
-  [[ $SOURCE != /* ]] && SOURCE=$DIR/$SOURCE # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
-done
-SCRIPTDIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
-CONTENTSDIR=$SCRIPTDIR/../
+if [ ! -d /opt/retropie ]; then
+	echo "RetroPie not found!"
+	exit 1
+fi
 
-sudo -u pi cp -rv $CONTENTSDIR/filesystem/ /
-chmod -R a+x /opt/retropie/configs/all/emulationstation/scripts/*.sh
+mkdir -p /opt/retropie/configs/all/emulationstation/scripts/sleep/
+mkdir -p /opt/retropie/configs/all/emulationstation/scripts/wake/
+mkdir -p /opt/retropie/configs/all/emulationstation/scripts/screensaver-stop/
+mkdir -p /opt/retropie/configs/all/emulationstation/scripts/screensaver-start/
+
+ln -s /home/pi/Freeplay/Freeplay_joystick_i2c/scripts/lcd_off.sh /opt/retropie/configs/all/emulationstation/scripts/sleep/
+ln -s /home/pi/Freeplay/Freeplay_joystick_i2c/scripts/lcd_on.sh /opt/retropie/configs/all/emulationstation/scripts/wake/
+ln -s /home/pi/Freeplay/Freeplay_joystick_i2c/scripts/lcd_on.sh /opt/retropie/configs/all/emulationstation/scripts/screensaver-stop/
+ln -s /home/pi/Freeplay/Freeplay_joystick_i2c/scripts/lcd_dim.sh /opt/retropie/configs/all/emulationstation/scripts/screensaver-start/
+
+chmod -R a+x /opt/retropie/configs/all/emulationstation/scripts/
 
 if ! grep -Fxq "/home/pi/Freeplay/Freeplay_joystick_i2c/scripts/bootup.sh" /opt/retropie/configs/all/autostart.sh; then
 	sed -i '1 i /home/pi/Freeplay/Freeplay_joystick_i2c/scripts/bootup.sh' /opt/retropie/configs/all/autostart.sh
