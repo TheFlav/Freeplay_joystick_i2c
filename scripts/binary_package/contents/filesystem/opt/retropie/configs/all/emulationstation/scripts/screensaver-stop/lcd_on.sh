0decb8d69554019a211ea85eb576cbf697d448a2
Ed <freeplay@gmail.com>
2022-05-03 11:24:28 -0600

    adding LCD sleep/dim scripts to RetroPie

---
+++ scripts/binary_package/contents/filesystem/opt/retropie/configs/all/emulationstation/scripts/screensaver-stop/lcd_on.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+LCDINUSE=$(/opt/vc/bin/tvservice -s | grep -c "\[LCD\]")
+if [[ "$LCDINUSE" == "1" ]]; then
+  i2cset -y 0 0x40 0x0a 0x55
+  i2cset -y 0 0x40 0x02 0x00
+  i2cset -y 0 0x40 0x0a 0xAA
+fi
--- scripts/install_to_retropie.sh
+++ scripts/install_to_retropie.sh
@@ -1,3 +1,17 @@
+#!/usr/bin/env bash
+
+SOURCE=${BASH_SOURCE[0]}
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  SCRIPTDIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
+  SOURCE=$(readlink "$SOURCE")
+  [[ $SOURCE != /* ]] && SOURCE=$DIR/$SOURCE # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+SCRIPTDIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
+CONTENTSDIR=$SCRIPTDIR/../
+
+sudo -u pi cp -rv $CONTENTSDIR/filesystem/ /
+chmod -R a+x /opt/retropie/configs/all/emulationstation/scripts/*.sh
+
 if ! grep -Fxq "/home/pi/Freeplay/Freeplay_joystick_i2c/scripts/bootup.sh" /opt/retropie/configs/all/autostart.sh; then
 	sed -i '1 i /home/pi/Freeplay/Freeplay_joystick_i2c/scripts/bootup.sh' /opt/retropie/configs/all/autostart.sh
 fi
