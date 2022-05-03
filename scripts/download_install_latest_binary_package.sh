432ed99f158e8905877dd00ca7ce5d2f725cce20
Ed <freeplay@gmail.com>
2022-05-03 15:23:26 -0600

    adding script to download/install latest binary package

---
+++ scripts/download_install_latest_binary_package.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+wget -O /tmp/fpjoy_binary_package.zip $(curl -s https://api.github.com/repos/TheFlav/Freeplay_joystick_i2c/releases/latest | grep 'browser_' | cut -d\" -f4)
+cd /tmp
+unzip fpjoy_binary_package.zip
+mv /home/pi/Freeplay/Freeplay_joystick_i2c /home/pi/Freeplay/Freeplay_joystick_i2c_BAK_$(date +"%Y-%m-%d-%H-%M-%S")
+/tmp/fpjoy/install_binary_package.sh
