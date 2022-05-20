74583d74148011780f4086c2f8f6d53a597766d7
Ed Mandy <freeplay@gmail.com>
2022-05-20 16:16:15 -0600

    Pi Zero was enabling serial console output during bootup.

--- scripts/setup_linux.sh
+++ scripts/setup_linux.sh
@@ -1,4 +1,6 @@
 sudo raspi-config nonint do_i2c 0
+sudo raspi-config nonint do_serial 1
+sudo raspi-config nonint set_config_var enable_uart 1 /boot/config.txt
 #sudo apt install i2c-tools libi2c-dev wiringpi
 #if ! grep -Fxq "/etc/modules" i2c-dev; then
 #	sed -i '1 i /etc/modules' i2c-dev
